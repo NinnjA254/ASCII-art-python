43db45924ef4a444b7f844d9cae044d0bf9b7b2a
NinnjA254 <quaypeter@yahoo.com>
2023-03-08 22:47:51 +0300

    compute a single brightness value for each pixel

--- main.py
+++ main.py
@@ -5,7 +5,24 @@ img = Image.open("sample.jpg")
 width = img.size[0]
 height = img.size[1]
 data = list(img.getdata())
+print(data[123])
 
+#compute a single brightness value for each pixel(an RGB tuple) #todo: try out the different "algorithms", perhaps allow the user to
+# pick between em
+#average: regular average
+for j, el in enumerate(data):
+    data[j] = (el[0] + el[1] + el[2]) / 3 
+
+# #luminosity: take a weighted average of the R, G and B values to account for human perception 
+# for j, el in enumerate(data):
+#     data[j] = (0.21 * el[0]) + (0.72 * el[1]) + (0.07 * el[2]) 
+
+# # lightness: average the maximum and minimum values out of R, G and B
+# for j, el in enumerate(data):
+#     data[j] = (max(el[0], el[1], el[2]) + min(el[0], el[1], el[2])) / 2 
+print(data[123])
+
+#unflatten the data
 unflattened_data = [[]]
 column = 0
 for j, el in enumerate(data):
@@ -13,3 +30,4 @@ for j, el in enumerate(data):
         column += 1
         unflattened_data.append([])
     unflattened_data[column].append(el)
+
---
+++ nodetoself.md
@@ -0,0 +1,3 @@
+<!-- @format -->
+
+swap sample image for a 640x480 image. test the difference
