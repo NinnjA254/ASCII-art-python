6f070739cde23d11c573d8c37c4c35a6f017dd71
NinnjA254 <quaypeter@yahoo.com>
2023-03-10 20:57:42 +0300

    put calculating brightness into a function

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from PIL import Image
 
 # get pixel data from image
-img = Image.open("deeznuts.jpg")
+img = Image.open("spidey.png")
 width = img.size[0]
 height = img.size[1]
 data = list(img.getdata())
 
-#compute a single brightness value for each pixel(an RGB tupleo) #todo: try out the different "algorithms", perhaps allow the user to
-# pick between em
-#average: regular average
-for j, el in enumerate(data):
-    data[j] = round((el[0] + el[1] + el[2]) / 3) 
+def compute_brightness_values(img_data, algo):
+    if (algo == 1):
+        #average: regular average
+        for j, el in enumerate(img_data):
+           img_data[j] = round((el[0] + el[1] + el[2]) / 3) 
+        return
+    if algo == 2:
+        #luminosity: take a weighted average of the R, G and B values to account for human perception 
+        for j, el in enumerate(img_data):
+           img_data[j] = round((0.21 * el[0]) + (0.72 * el[1]) + (0.07 * el[2])) 
+        return
+    if algo == 3: # lightness: average the maximum and minimum values out of R, G and B
+        for j, el in enumerate(img_data):
+           img_data[j] = round((max(el[0], el[1], el[2]) + min(el[0], el[1], el[2])) / 2) 
+compute_brightness_values(data,2) 
 
-# #luminosity: take a weighted average of the R, G and B values to account for human perception 
-# for j, el in enumerate(data):
-#     data[j] = round((0.21 * el[0]) + (0.72 * el[1]) + (0.07 * el[2])) 
-
-# # lightness: average the maximum and minimum values out of R, G and B
-# for j, el in enumerate(data):
-#     data[j] = round((max(el[0], el[1], el[2]) + min(el[0], el[1], el[2])) / 2) 
 
 # make ascii representation of the pixel data
 ascii_map = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
